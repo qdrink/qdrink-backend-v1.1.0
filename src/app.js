33d76965cb58fc94a46226488a08cad8d4d6a210
Diego Medel <medeldiegote@gmail.com>
2023-11-23 12:09:17 -0300

    se agrego cors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ import transaccionesRoutes from "./routes/transactions";
 
 const app = express();
 
+//cors
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  }
+  
+app.use(allowCrossDomain);
+
 // settings
 app.set("port", process.env.PORT || 8090);
 
